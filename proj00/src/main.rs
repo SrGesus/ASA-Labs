a792b073a6a658423d00d64da40da83bea7dc61a
SrGesus <108523575+SrGesus@users.noreply.github.com>
2023-11-14 16:18:15 +0000

    Fixed parse_line macro

--- proj00/src/main.rs
+++ proj00/src/main.rs
@@ -1,61 +1,56 @@
 use std::vec::Vec;
 use std::io::{self, BufReader, BufRead};
 
-// macro_rules! parse_line {
-//     (&String: separator, $($t: ty),+) => ({
-//         let mut a_str = String::new();
-//         io::stdin().read_line(&mut a_str).expect("read error");
-//         let mut a_iter = a_str.split(separator);
-//         (
-//             $(
-//             a_iter.next().unwrap().parse::<$t>().expect("parse error"),
-//             )+
-//         )
-//     })
-// }
+macro_rules! parse_line {
+    ($separator: literal, $($t: ty),+) => ({
+        let mut a_str = String::new();
+        io::stdin().read_line(&mut a_str).expect("read error");
+        a_str.pop();
+        let mut a_iter = a_str.split($separator);
+        (
+            $(
+                a_iter.next().unwrap().parse::<$t>().expect("parse error"),
+            )+
+        )
+    })
+}
 
 #[derive(Clone, Debug)]
 struct Person {
     my_friends: Vec<usize>,
-    friend_of: Vec<usize>,
+    friend_of: usize,
 }
 
 impl Person {
     pub fn new() -> Self {
         Self {
             my_friends: Vec::new(),
-            friend_of: Vec::new(),
+            friend_of: 0,
         }
     }
 }
 
 fn main() {
-    let stdin = io::stdin();
-    let mut reader = BufReader::new(stdin).lines();
-
-    let line1 = reader.next().unwrap().unwrap();
-    let mut line1 = line1.split(",");
-    let num_people = line1.next().unwrap().parse::<usize>().unwrap();
-    let num_relations = line1.next().unwrap().parse::<usize>().unwrap();
+    let (num_people, _num_relations) = parse_line!(",", usize, usize);
 
     let mut people: Vec<Person> = vec!(Person::new(); num_people);
-
-    for line in reader.map(|l| l.unwrap()) {
+    
+    for line in BufReader::new(io::stdin()).lines().map(|l| l.unwrap()) {
         let mut data = line.split(" ");
         let person1 = data.next().unwrap().parse::<usize>().unwrap();
         let person2 = data.next().unwrap().parse::<usize>().unwrap();
         people.get_mut(person1-1).unwrap().my_friends.push(person2-1);
-        people.get_mut(person2-1).unwrap().friend_of.push(person1-1);
+        people[person2-1].friend_of += 1;
     }
 
-    // Pessoas com i amigas
+    // Num Pessoas com i amigas
     let mut histograma1: Vec<usize> = vec!(0; num_people);
-    // Pessoas que são amigas de i pessoas
+    // Num Pessoas que são amigas de i pessoas
     let mut histograma2: Vec<usize> = vec!(0; num_people);
 
     for p in &people {
-        histograma1[p.my_friends.len()] = histograma1[p.my_friends.len()]+1;
-        histograma2[p.friend_of.len()] = histograma2[p.friend_of.len()]+1;
+        histograma1[p.my_friends.len()] += 1;
+        histograma2[p.friend_of] += 1;
     }
 
     println!("Histograma 1\n{:?}", histograma1);

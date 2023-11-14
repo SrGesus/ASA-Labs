c55f5b4753013e607ae7ce94f5f5b10b0ae69126
SrGesus <108523575+SrGesus@users.noreply.github.com>
2023-11-14 14:52:14 +0000

    Removed rustc on shell.nix

--- shell.nix
+++ shell.nix
@@ -4,7 +4,6 @@ pkgs.mkShell {
   # were you to to use nix-build not nix-shell and build whatever you were working on
   CARGO_HOME = toString ./. + "/.cargo_home";
   buildInputs = [
-    pkgs.rustc
     pkgs.cargo
   ];
 }
